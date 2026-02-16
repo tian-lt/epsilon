669c27126744e7247f2987752a4d4d6793b2cdc1
TIAN LIAO <tian.lt@outlook.com>
2026-02-16 20:50:01 +0800

    to_string (#9)
    * to decimal string
    
    * parse signs
    
    * fix clang + gcc

--- src/epsilon/chars.hpp
+++ src/epsilon/chars.hpp
@@ -20,7 +20,25 @@ std::optional<z<C>> try_from_chars(std::string_view chars) {
   using D = typename z<C>::digit_type;
 
   if constexpr (B == 10) {
-    z<C> ten = {.digits = {10}};
+    const z<C> ten = {.digits = {10}};
+
+    sign sgn;
+    if (chars.length() > 1) {
+      if (chars.front() == '+' && isdigit(static_cast<unsigned int>(chars[1]))) {
+        sgn = sign::positive;
+        chars = chars.substr(1);
+      } else if (chars.front() == '-' && isdigit(static_cast<unsigned int>(chars[1]))) {
+        sgn = sign::negative;
+        chars = chars.substr(1);
+      } else if (isdigit(static_cast<unsigned int>(chars.front()))) {
+        sgn = sign::positive;
+      } else {
+        return std::nullopt;
+      }
+    } else {
+      sgn = sign::positive;
+    }
+
     z<C> res;
     for (auto ch : chars) {
       if (!isdigit(static_cast<unsigned int>(ch))) {
@@ -33,6 +51,38 @@ std::optional<z<C>> try_from_chars(std::string_view chars) {
       }
       res = res * ten + tmp;
     }
+    if (!is_zero(res)) {
+      res.sgn = sgn;
+    }
+    return res;
+  } else {
+    static_assert("not implemented.");
+  }
+}
+
+template <container C, int B = 10>
+std::string to_string(z<C> num) {
+  if constexpr (B == 10) {
+    if (is_zero(num)) {
+      return "0";
+    }
+
+    std::string res;
+    sign sgn = num.sgn;
+    const z<C> ten = {.digits = {10}};
+    while (!is_zero(num)) {
+      auto [q, r] = div_n(num, ten);
+      if (is_zero(r)) {
+        res.push_back('0');
+      } else {
+        res.push_back(static_cast<char>(r.digits[0]) + '0');
+      }
+      num = std::move(q);
+    }
+    if (sgn == sign::negative) {
+      res.push_back('-');
+    }
+    std::ranges::reverse(res);
     return res;
   } else {
     static_assert("not implemented.");
--- src/ut/chars_tests.cpp
+++ src/ut/chars_tests.cpp
@@ -18,15 +18,47 @@ TEST(chars_tests, try_from_decimal_chars) {
     auto opt_num = epx::try_from_chars<sz::container_type>("");
     EXPECT_TRUE(opt_num.has_value());
     EXPECT_TRUE(epx::is_zero(*opt_num));
+    EXPECT_TRUE(epx::is_positive(*opt_num));
   }
   {
     auto opt_num = epx::try_from_chars<sz::container_type>("0");
     EXPECT_TRUE(opt_num.has_value());
     EXPECT_TRUE(epx::is_zero(*opt_num));
+    EXPECT_TRUE(epx::is_positive(*opt_num));
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("-0");
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_TRUE(epx::is_zero(*opt_num));
+    EXPECT_TRUE(epx::is_positive(*opt_num));
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("000000");
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_TRUE(epx::is_zero(*opt_num));
+    EXPECT_TRUE(epx::is_positive(*opt_num));
   }
   {
     auto opt_num = epx::try_from_chars<sz::container_type>("1");
     sz expected = {.digits = {1}};
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_EQ(expected, *opt_num);
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("-1");
+    sz expected = {.digits = {1}, .sgn = epx::sign::negative};
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_EQ(expected, *opt_num);
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("000001");
+    sz expected = {.digits = {1}};
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_EQ(expected, *opt_num);
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("-000001");
+    sz expected = {.digits = {1}, .sgn = epx::sign::negative};
     EXPECT_TRUE(opt_num.has_value());
     EXPECT_EQ(expected, *opt_num);
   }
@@ -66,6 +98,12 @@ TEST(chars_tests, try_from_decimal_chars) {
     EXPECT_TRUE(opt_num.has_value());
     EXPECT_EQ(expected, *opt_num);
   }
+  {
+    auto opt_num = epx::try_from_chars<mz::container_type>("-4294967296");
+    mz expected = {.digits = {0, 0, 1}, .sgn = epx::sign::negative};
+    EXPECT_TRUE(opt_num.has_value());
+    EXPECT_EQ(expected, *opt_num);
+  }
 }
 
 TEST(chars_tests, try_from_chars_bad_chars) {
@@ -82,16 +120,93 @@ TEST(chars_tests, try_from_chars_bad_chars) {
     EXPECT_FALSE(opt_num.has_value());
   }
   {
-    auto opt_num = epx::try_from_chars<sz::container_type>("+1");
-    EXPECT_FALSE(opt_num.has_value());
-  }
-  {
-    auto opt_num = epx::try_from_chars<sz::container_type>("-1");
-    EXPECT_FALSE(opt_num.has_value());
-  }
-  {
     auto opt_num = epx::try_from_chars<sz::container_type>("12.34");
     EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("+");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("++1");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("+a");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("-");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("--2");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+  {
+    auto opt_num = epx::try_from_chars<sz::container_type>("-b");
+    EXPECT_FALSE(opt_num.has_value());
+  }
+}
+
+TEST(chars_tests, to_decimal_string) {
+  {
+    auto num = epx::try_from_chars<sz::container_type>("").value();
+    EXPECT_TRUE(epx::is_zero(num));
+    auto str = epx::to_string(num);
+    EXPECT_EQ("0", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("0").value();
+    EXPECT_TRUE(epx::is_zero(num));
+    auto str = epx::to_string(num);
+    EXPECT_EQ("0", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("-0").value();
+    EXPECT_TRUE(epx::is_zero(num));
+    auto str = epx::to_string(num);
+    EXPECT_EQ("0", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("0000").value();
+    EXPECT_TRUE(epx::is_zero(num));
+    auto str = epx::to_string(num);
+    EXPECT_EQ("0", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("1").value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("1", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("-1").value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("-1", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("-0000100").value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("-100", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("12345678910").value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("12345678910", str);
+  }
+  {
+    auto num = epx::try_from_chars<sz::container_type>("-12345678910").value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("-12345678910", str);
+  }
+  {
+    auto num = epx::try_from_chars<mz::container_type>(
+                   "000009837417236498716239487169238746192783461928736491287364192873461928736416341997812934781692387"
+                   "46192374")
+                   .value();
+    auto str = epx::to_string(num);
+    EXPECT_EQ("983741723649871623948716923874619278346192873649128736419287346192873641634199781293478169238746192374",
+              str);
   }
 }
 
